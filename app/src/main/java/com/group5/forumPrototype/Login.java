4f25817156ad014e6af8d0875e0c30a65e26ce89
CelmiraStrath <celmira.sebastiao.2020@uni.strath.ac.uk>
2021-03-28 21:49:55 +0100

    added the link for login, recover password/username buttons ...Added also a link for logout option of the select module spinner

--- app/src/main/java/com/group5/forumPrototype/Login.java
+++ app/src/main/java/com/group5/forumPrototype/Login.java
@@ -2,13 +2,73 @@ package com.group5.forumPrototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+
+
+
+    Button loginBtn;
+    Button forgotEmailBtn;
+    Button forgotPassnBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        loginBtn = findViewById(R.id.button2);
+        forgotEmailBtn = findViewById(R.id.button3);
+        forgotPassnBtn = findViewById(R.id.button4);
+        //event is generated when the user clicks login button
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //back to homepage with elements initialized
+                Intent intent= new Intent(Login.this,SelectModule.class);
+                startActivity(intent);
+
+            }
+
+
+        });
+
+        forgotEmailBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //redirect the user to recover password page
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.strath.ac.uk/professionalservices/it/use/password/"));
+                startActivity(intent);
+
+            }
+
+
+        });
+
+        forgotPassnBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //redirect the user to recover password page
+                Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.strath.ac.uk/professionalservices/it/use/password/"));
+                startActivity(intent);
+
+
+            }
+
+
+        });
+
     }
 }
--- app/src/main/java/com/group5/forumPrototype/SelectModule.java
+++ app/src/main/java/com/group5/forumPrototype/SelectModule.java
@@ -2,6 +2,7 @@ package com.group5.forumPrototype;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,6 +14,8 @@ public class SelectModule extends Activity {
     Spinner studentSpinner;
     ArrayAdapter<CharSequence> studentAdapter;
     Button cs991Button;
+    Button logoutBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +33,24 @@ public class SelectModule extends Activity {
         studentAdapter = ArrayAdapter.createFromResource(this, R.array.student_spinner, android.R.layout.simple_spinner_item);
         studentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         studentSpinner.setAdapter(studentAdapter);
+
+        // to complete
+       // logoutBtn = findViewById(R.id.);
+
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //back to homepage with elements initialized
+                Intent intent= new Intent(SelectModule.this,Login.class);
+                startActivity(intent);
+
+            }
+
+
+        });
     }
 
     private void openPostPage() {
